d4a0a68fe8414154b3668f9323c4e840cfd0e2b1
Shwetha Prakash <shwetha.prakash@gmail.com>
2021-03-15 13:40:31 -0700

    Updates

---
+++ precache-manifest.422be96ccc45f2be97890d69ad5537e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8e75e9ecf849e955f17d765cf54dc3",
+    "url": "/WorldCovidStats/index.html"
+  },
+  {
+    "revision": "6fb8922b482a8c08e994",
+    "url": "/WorldCovidStats/static/css/2.c1fb602b.chunk.css"
+  },
+  {
+    "revision": "6fb8922b482a8c08e994",
+    "url": "/WorldCovidStats/static/js/2.f62e4b4f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/WorldCovidStats/static/js/2.f62e4b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233fbdffb3ea1b6b2309",
+    "url": "/WorldCovidStats/static/js/main.7067d7b7.chunk.js"
+  },
+  {
+    "revision": "a84a6709eac8871c580c",
+    "url": "/WorldCovidStats/static/js/runtime-main.27a94bf6.js"
+  }
+]);
